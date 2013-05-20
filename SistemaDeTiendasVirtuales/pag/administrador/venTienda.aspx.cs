3924640a1a7dae1b13e12c514837464ce66a310d
ezequielF <ezequiel.falleau@gmail.com>
2013-05-19 23:37:29 -0300

    Carpeta administrador y logReg

---
+++ SistemaDeTiendasVirtuales/pag/administrador/venTienda.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SistemaDeTiendasVirtuales.pag.administrador
+{
+    public partial class venTienda : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
